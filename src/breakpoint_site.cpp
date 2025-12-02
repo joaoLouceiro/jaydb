63b5a8c09688ef6a173fd96d30ca8afa58bd979e
joaoLouceiro <jrlouceiro@live.com>
2025-12-02 23:20:17 +0000

    Add breakpoint_site::enable() and ::disable()

--- src/breakpoint_site.cpp
+++ src/breakpoint_site.cpp
@@ -1,5 +1,10 @@
-#include "libjdb/types.hpp"
+#include <cerrno>
+#include <cstddef>
+#include <cstdint>
 #include <libjdb/breakpoint_site.hpp>
+#include <libjdb/process.hpp>
+#include <libjdb/types.hpp>
+#include <sys/ptrace.h>
 
 namespace {
 auto get_next_id() {
@@ -11,4 +16,45 @@ auto get_next_id() {
 jdb::breakpoint_site::breakpoint_site(process &proc, virt_addr address)
     : process_{&proc}, address_{address}, is_enabled_{false}, saved_data_{} {
     id_ = get_next_id();
+}
+
+void jdb::breakpoint_site::enable() {
+    if (is_enabled_)
+        return;
+
+    errno = 0;
+    std::uint64_t data = ptrace(PTRACE_PEEKDATA, process_->pid(), address_, nullptr);
+    if (errno != 0) {
+        error::send_errno("Enabling breakpoint site failed");
+    }
+
+    saved_data_ = static_cast<std::byte>(data & 0xff);
+
+    std::uint64_t int3 = 0xcc;
+    std::uint64_t data_with_int3 = ((data & ~0xff) | int3);
+
+    if (ptrace(PTRACE_POKEDATA, process_->pid(), address_, data_with_int3) < 0) {
+        error::send_errno("Enabling breakpoint site failed");
+    }
+
+    is_enabled_ = true;
+}
+
+void jdb::breakpoint_site::disable() {
+    if (!is_enabled_)
+        return;
+
+    errno = 0;
+    std::uint64_t data = ptrace(PTRACE_PEEKDATA, process_->pid(), address_, nullptr);
+    if (errno != 0) {
+        error::send_errno("Disabling breakpoint site failed");
+    }
+
+    auto restored_data = ((data & ~0xff) | static_cast<std::uint8_t>(saved_data_));
+
+    if (ptrace(PTRACE_POKEDATA, process_->pid(), address_, restored_data) < 0) {
+        error::send_errno("Disabling breakpoint site failed");
+    }
+
+    is_enabled_ = false;
 }
